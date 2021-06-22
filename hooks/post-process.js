5b1858ebe985cd90de6327d28570b8bc49af202c
Michael Davis <michael@damaru.com>
2021-06-22 10:54:11 -0400

    feat: upgraded library versions, better support for Solace queues (#119)
    * We how handle smf bindings properly.
    
    * Updated readme, fixed linter problems.
    
    * Simplified functions to make sonarcloud happy.
    
    * Fixed linter problems.
    
    * Removed unused topicInfo filter.
    
    * Removed dead code.
    
    Co-authored-by: Michael Davis <michael.davis@solace.com>

--- hooks/post-process.js
+++ hooks/post-process.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 const ScsLib = require('../lib/scsLib.js');
+// To enable debug logging, set the env var DEBUG="postProcess" with whatever things you want to see.
+const debugPostProcess = require('debug')('postProcess');
 
 const sourceHead = '/src/main/java/';
 
@@ -13,41 +15,31 @@ module.exports = {
     let sourcePath = generator.targetDir + sourceHead;
     const info = asyncapi.info();
     let package = generator.templateParams['javaPackage'];
-    let generateMessagingClass = false;
-    const gen = generator.templateParams['generateMessagingClass'];
     const extensions = info.extensions();
-
-    if (gen === 'true') {
-      generateMessagingClass = true;
-    }
 
     if (!package && info && extensions) {
       package = extensions['x-java-package'];
     }
 
     if (package) {
-      //console.log("package: " + package);
+      debugPostProcess(`package: ${package}`);
       const overridePath = `${generator.targetDir + sourceHead + package.replace(/\./g, '/')}/`;
-      //console.log("Moving files from " + sourcePath + " to " + overridePath);
+      debugPostProcess(`Moving files from ${sourcePath} to ${overridePath}`);
       let first = true;
       fs.readdirSync(sourcePath).forEach(file => {
         if (!fs.lstatSync(path.resolve(sourcePath, file)).isDirectory()) {
           if (first) {
             first = false;
-            //console.log("Making " + overridePath);
+            debugPostProcess(`Making ${overridePath}`);
             fs.mkdirSync(overridePath, { recursive: true });
           }
 
-          if ((file !== 'Messaging.java') || generateMessagingClass) {
-            //console.log("Copying " + file)
-            fs.copyFileSync(path.resolve(sourcePath, file), path.resolve(overridePath, file));
-          }
+          debugPostProcess(`Copying ${file}`);
+          fs.copyFileSync(path.resolve(sourcePath, file), path.resolve(overridePath, file));
           fs.unlinkSync(path.resolve(sourcePath, file));
         }
       });
       sourcePath = overridePath;
-    } else if (!generateMessagingClass) {
-      fs.unlinkSync(path.resolve(sourcePath, 'Messaging.java'));
     }
 
     // Rename the pom file if necessary, and only include Application.java when an app is requested.
@@ -59,7 +51,6 @@ module.exports = {
       fs.renameSync(path.resolve(generator.targetDir, 'pom.lib'), path.resolve(generator.targetDir, 'pom.xml'));
       fs.unlinkSync(path.resolve(generator.targetDir, 'pom.app'));
       fs.unlinkSync(path.resolve(sourcePath, 'Application.java'));
-      //fs.unlinkSync(path.resolve(sourcePath, "ApplicationWithMessaging.java"));
     } else {
       fs.renameSync(path.resolve(generator.targetDir, 'pom.app'), path.resolve(generator.targetDir, 'pom.xml'));
       fs.unlinkSync(path.resolve(generator.targetDir, 'pom.lib'));
@@ -73,49 +64,28 @@ module.exports = {
     // This renames schema objects ensuring they're proper Java class names. It also removes files that are schemas of simple types.
 
     const schemas = asyncapi.components().schemas();
-    //console.log("schemas: " + JSON.stringify(schemas));
+    debugPostProcess('schemas:');
+    debugPostProcess(schemas);
 
-    for (const schema in schemas) {
-      const schemaObj = schemas[schema];
-      const type = schemaObj.type();
-      const oldPath = path.resolve(sourcePath, `${schema}.java`);
-
-      //console.log(`postprocess schema ${schema} ${type}`);
+    for (const schemaName in asyncapi.components().schemas()) {
+      const schema = schemas[schemaName];
+      const type = schema.type();
+      debugPostProcess(`postprocess schema ${schemaName} ${type}`);
+      const oldPath = path.resolve(sourcePath, `${schemaName}.java`);
 
       if (type === 'object' || type === 'enum') {
-        let javaName = _.camelCase(schema);
+        let javaName = _.camelCase(schemaName);
         javaName = _.upperFirst(javaName);
 
-        if (javaName !== schema) {
+        if (javaName !== schemaName) {
           const newPath = path.resolve(sourcePath, `${javaName}.java`);
           fs.renameSync(oldPath, newPath);
-          // console.log("Renamed class file "  + schema + " to " + javaName);
+          debugPostProcess(`Renamed class file ${schemaName} to ${javaName}`);
         }
       } else {
         fs.unlinkSync(oldPath);
       }
     }
-
-    // This renames schema objects according to the title field. By default we won't do this, we might add this as an option.
-
-    //const schemas = asyncapi.components().schemas();
-    //console.log("schemas: " + JSON.stringify(schemas));
-
-    /*
-    for (let schema in schemas) {
-      let schemaObject = schemas[schema];
-      let title = _.upperFirst(schemaObject.title);
-      console.log("schema "  + schema + " title: " + title);
-      if (title && title != schema) {
-        let pathBySchema = path.resolve(sourcePath, schema + ".java");
-        let pathByTitle = path.resolve(sourcePath, title + ".java");
-
-        if (fs.existsSync(pathBySchema) && !fs.existsSync(pathByTitle)) {
-          fs.renameSync(pathBySchema, pathByTitle);
-        }
-      }
-    }
-    */
   }
 };
 
