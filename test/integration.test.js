402d91d78c96394e1cf4111a205e247a97e59ac3
CameronRushton <cameron.rushton@solace.com>
2021-06-28 10:38:42 -0400

    test: removed husky precommits; removed coverage setup for later PR; reverted eslintrc rename; use snapshot testing

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,68 +1,45 @@
 const path = require('path');
 const Generator = require('@asyncapi/generator');
-const fs = require('fs');
-const yaml = require('js-yaml');
-const { test } = require('@jest/globals');
+const { readFile } = require('fs').promises;
+const crypto = require('crypto');
+
+const MAIN_TEST_RESULT_PATH = 'test/temp/integrationTestResult';
 
 describe('template integration tests using the generator', () => {
-  const OUTPUT_FOLDER = 'test_output';
-
-  const fileExists = (file) => {
-    return new Promise((resolve) => {
-      fs.access(file, fs.constants.F_OK, (err) => {
-        return err ? resolve(file) : resolve(true);
-      });
-    });
+  const generateFolderName = () => {
+    // you always want to generate to new directory to make sure test runs in clear environment
+    return path.resolve(MAIN_TEST_RESULT_PATH, crypto.randomBytes(4).toString('hex'));
   };
 
-  beforeEach(async () => {
-    this.params = {
+  jest.setTimeout(30000);
+
+  it('should generate application files using the solace binder', async () => {
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.acme';
+    const PACKAGE_PATH = PACKAGE.replace('.', '/');
+    const params = {
       binder: 'solace',
-      javaPackage: 'com.acme',
+      javaPackage: PACKAGE,
       host: 'testVmrUri',
       username: 'user',
-      password: 'password',
+      password: 'test',
       msgVpn: 'vpnName',
       artifactId: 'asyncApiFileName'
     };
-    const PACKAGE = 'com.acme';
-    this.packagePathTokens = `${PACKAGE.replace('.', '/')}/`;
-    const generator = new Generator('./', path.resolve('test', OUTPUT_FOLDER), { templateParams: this.params });
+    
+    const generator = new Generator('./', OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/solace-test-app.yaml'));
 
-    try {
-      await generator.generateFromFile(path.resolve('test', 'mocks/solace-test-app.yaml'));
-    } catch (e) {
-      fs.rmdirSync(path.resolve('test', OUTPUT_FOLDER), { recursive: true });
-      throw new Error(e);
+    const expectedFiles = [
+      'pom.xml',
+      'README.md',
+      `src/main/java/${PACKAGE_PATH}/Application.java`,
+      `src/main/java/${PACKAGE_PATH}/MySchema.java`,
+      'src/main/resources/application.yml'
+    ];
+    for (let index in expectedFiles) {
+      const file = await readFile(path.join(OUTPUT_DIR, expectedFiles[index]), 'utf8');
+      expect(file).toMatchSnapshot();
     }
   });
-
-  afterEach(() => {
-    fs.rmdirSync(path.resolve('test', OUTPUT_FOLDER), { recursive: true });
-  });
-
-  test('template generates application files', async () => {
-    const expectedFiles = ['pom.xml', 'README.md', `src/main/java/${this.packagePathTokens}Application.java`, 'src/main/resources/application.yml'];
-    const promises = [];
-    expectedFiles.forEach(filePath => promises.push(fileExists(path.resolve('test', OUTPUT_FOLDER, filePath))));
-    try {
-      const results = await Promise.all(promises);
-      const filesFailedToFind = results.filter(element => element !== true);
-      expect(filesFailedToFind.length).toBe(0);
-    } catch (e) {
-      throw new Error(e);
-    }
-  }, 30000);
-
-  test('sets vmr properties in application yaml for solace binder', () => {
-    const applicationYaml = yaml.load(fs.readFileSync(path.resolve('test', OUTPUT_FOLDER, 'src/main/resources/application.yml'), 'utf8'));
-    const actualHost = applicationYaml.spring.cloud.stream.binders['solace-binder'].environment.solace.java.host;
-    const actualMsgVpn = applicationYaml.spring.cloud.stream.binders['solace-binder'].environment.solace.java.msgVpn;
-    const actualUsername = applicationYaml.spring.cloud.stream.binders['solace-binder'].environment.solace.java.clientUsername;
-    const actualPassword = applicationYaml.spring.cloud.stream.binders['solace-binder'].environment.solace.java.clientPassword;
-    expect(this.params.host).toBe(actualHost);
-    expect(this.params.msgVpn).toBe(actualMsgVpn);
-    expect(this.params.username).toBe(actualUsername);
-    expect(this.params.password).toBe(actualPassword);
-  }, 30000);
 });
