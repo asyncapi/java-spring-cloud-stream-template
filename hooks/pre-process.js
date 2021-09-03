1ad373b897f3c44d30612e87f15b0ba8677869bc
Michael Davis <michael.davis@solace.com>
2021-09-03 11:33:14 -0400

    Removed file that wasn't supposed to be checked in.

--- hooks/pre-process.js
+++
@@ -1,7 +0,0 @@
-const ApplicationModel = require('../lib/applicationModel.js');
-
-module.exports = {
-  'generate:before': generator => {
-    ApplicationModel.asyncapi = generator.asyncapi;
-  }
-};
