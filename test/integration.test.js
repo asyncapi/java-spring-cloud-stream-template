e23b52544a4cbf8edb737c1fd5cd932314e2aaa5
CameronRushton <cameron.rushton@solace.com>
2022-02-24 16:31:30 -0500

    clean up integration tests; move common code into their own homes

--- test/integration.test.js
+++ test/integration.test.js
@@ -3,15 +3,28 @@ const Generator = require('@asyncapi/generator');
 const { readFile } = require('fs').promises;
 const crypto = require('crypto');
 
-const MAIN_TEST_RESULT_PATH = path.join('test', 'temp', 'integrationTestResult');
+// Constants not overridden per test
+const TEST_FOLDER_NAME = 'test';
+const MAIN_TEST_RESULT_PATH = path.join(TEST_FOLDER_NAME, 'temp', 'integrationTestResult');
 
 describe('template integration tests using the generator', () => {
   jest.setTimeout(30000);
 
+  // Constants that may be overridden per test
+  const DEFAULT_PACKAGE = 'com.acme';
+  const DEFAULT_PACKAGE_PATH = path.join(...DEFAULT_PACKAGE.split('.'));
+
+  let outputDirectory;
+
   const generateFolderName = () => {
-    // you always want to generate to new directory to make sure test runs in clear environment
+    // we always want to generate to new directory to make sure test runs in clear environment
     return path.resolve(MAIN_TEST_RESULT_PATH, crypto.randomBytes(4).toString('hex'));
   };
+
+  const generate = async (asyncApiFilePath, params) => {
+    const generator = new Generator(path.normalize('./'), outputDirectory, { forceWrite: true, templateParams: params });
+    return await generator.generateFromFile(path.resolve(TEST_FOLDER_NAME, asyncApiFilePath));
+  }
 
   const assertExpectedFiles = async (outputDirectory, expectedFiles) => {
     for (const index in expectedFiles) {
@@ -20,64 +33,55 @@ describe('template integration tests using the generator', () => {
     }
   };
 
+  beforeEach(() => {
+    outputDirectory = generateFolderName();
+  });
+
   it('should generate application files using the solace binder', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    const PACKAGE = 'com.acme';
-    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
     const params = {
       binder: 'solace',
-      javaPackage: PACKAGE,
+      javaPackage: DEFAULT_PACKAGE,
       host: 'testVmrUri',
       username: 'user',
       password: 'test', //NOSONAR
       msgVpn: 'vpnName',
       artifactId: 'asyncApiFileName'
     };
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
-    await generator.generateFromFile(path.resolve('test', 'mocks/solace-test-app.yaml'));
 
-    const expectedFiles = [
+    await generate('mocks/solace-test-app.yaml', params);
+
+    const validatedFiles = [
       'pom.xml',
       'README.md',
-      `src/main/java/${PACKAGE_PATH}/Application.java`,
-      `src/main/java/${PACKAGE_PATH}/MySchema.java`,
+      `src/main/java/${DEFAULT_PACKAGE_PATH}/Application.java`,
+      `src/main/java/${DEFAULT_PACKAGE_PATH}/MySchema.java`,
       'src/main/resources/application.yml'
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('should generate a consumer and return a payload when using x-scs-function-name and dynamic topic binding', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true });
-    await generator.generateFromFile(path.resolve('test', 'mocks/scs-function-name/dynamic-topic-same-function-name.yaml'));
+    await generate('mocks/scs-function-name/dynamic-topic-same-function-name.yaml');
   
-    const expectedFiles = [
+    const validatedFiles = [
       'src/main/java/Application.java'
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('should generate a function and return a payload when using x-scs-function-name and a static topic', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true });
-    await generator.generateFromFile(path.resolve('test', 'mocks/scs-function-name/animals-same-function-name.yaml'));
-  
-    const expectedFiles = [
+    await generate('mocks/scs-function-name/animals-same-function-name.yaml');
+
+    const validatedFiles = [
       'src/main/java/Application.java'
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('should generate extra config when using the paramatersToHeaders parameter', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    const PACKAGE = 'com.acme';
-    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
     const params = {
       binder: 'solace',
-      javaPackage: PACKAGE,
+      javaPackage: DEFAULT_PACKAGE,
       host: 'testVmrUri',
       username: 'user',
       password: 'test', //NOSONAR
@@ -85,90 +89,70 @@ describe('template integration tests using the generator', () => {
       artifactId: 'asyncApiFileName',
       parametersToHeaders: true
     };
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
-    await generator.generateFromFile(path.resolve('test', 'mocks/solace-test-app.yaml'));
 
-    const expectedFiles = [
-      `src/main/java/${PACKAGE_PATH}/Application.java`,
+    await generate('mocks/solace-test-app.yaml', params);
+
+    const validatedFiles = [
+      `src/main/java/${DEFAULT_PACKAGE_PATH}/Application.java`,
       'src/main/resources/application.yml'
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('should generate a comment for a consumer receiving multiple messages', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true });
-    await generator.generateFromFile(path.resolve('test', 'mocks/animals.yaml'));
+    await generate('mocks/animals.yaml');
 
-    const expectedFiles = [
+    const validatedFiles = [
       'src/main/java/Application.java'
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('avro schemas should appear in a package based on their namespace, if any.', async () => {
     // Note that this file has 2 Avro schemas named User, but one has the namespace 'userpublisher.'
-    const OUTPUT_DIR = generateFolderName();
-    const PACKAGE = 'com.acme';
-    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
     const AVRO_PACKAGE_PATH = 'userpublisher';
     const params = {
       binder: 'kafka',
-      javaPackage: PACKAGE,
+      javaPackage: DEFAULT_PACKAGE,
       artifactId: 'asyncApiFileName'
     };
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
-    await generator.generateFromFile(path.resolve('test', 'mocks/kafka-avro.yaml'));
+    await generate('mocks/kafka-avro.yaml', params);
 
-    const expectedFiles = [
-      `src/main/java/${PACKAGE_PATH}/User.java`,
+    const validatedFiles = [
+      `src/main/java/${DEFAULT_PACKAGE_PATH}/User.java`,
       `src/main/java/${AVRO_PACKAGE_PATH}/User.java`,
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('should generate a model subclass when it sees an allOf', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    const PACKAGE = 'com.acme';
-    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
     const params = {
-      javaPackage: PACKAGE,
+      javaPackage: DEFAULT_PACKAGE,
       artifactId: 'asyncApiFileName'
     };
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
-    await generator.generateFromFile(path.resolve('test', 'mocks/error-reporter.yaml'));
+    await generate('mocks/error-reporter.yaml', params);
 
-    const expectedFiles = [
-      `src/main/java/${PACKAGE_PATH}/ExtendedErrorModel.java`
+    const validatedFiles = [
+      `src/main/java/${DEFAULT_PACKAGE_PATH}/ExtendedErrorModel.java`
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('should generate schemas with nested arrays', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true });
-    await generator.generateFromFile(path.resolve('test', 'mocks/nested-arrays.yaml'));
+    await generate('mocks/nested-arrays.yaml');
 
-    const expectedFiles = [
+    const validatedFiles = [
       'src/main/java/Application.java',
       'src/main/java/Dossier.java',
       'src/main/java/Debtor.java'
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('should generate code from the smarty lighting streetlights example', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true });
-    await generator.generateFromFile(path.resolve('test', 'mocks/smarty-lighting-streetlights.yaml'));
+    await generate('mocks/smarty-lighting-streetlights.yaml');
 
-    const expectedFiles = [
+    const validatedFiles = [
       'src/main/java/Application.java',
       'src/main/java/DimLightPayload.java',
       'src/main/java/LightMeasuredPayload.java',
@@ -176,19 +160,16 @@ describe('template integration tests using the generator', () => {
       'src/main/java/TurnOnOffPayload.java',
       'src/main/java/SubObject.java'
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 
   it('should generate code using schemas that have $id set', async () => {
-    const OUTPUT_DIR = generateFolderName();
-    
-    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true });
-    await generator.generateFromFile(path.resolve('test', 'mocks/using-$id-field.yaml'));
+    await generate('mocks/using-$id-field.yaml');
 
-    const expectedFiles = [
+    const validatedFiles = [
       'src/main/java/Application.java',
       'src/main/java/DefaultMessageSchema.java'
     ];
-    await assertExpectedFiles(OUTPUT_DIR, expectedFiles);
+    await assertExpectedFiles(outputDirectory, validatedFiles);
   });
 });
